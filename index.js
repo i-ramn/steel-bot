d49841790f0a90f6238f373305e8f590c417afc6
Igor Ramnyonak <sloed95@gmail.com>
2022-10-16 02:22:14 +0300

    feat: changed logic

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const TelegramApi = require("node-telegram-bot-api");
 const fs = require("fs");
 const files = fs.readdirSync("public/images");
 
+const chosenFile = () => files[Math.floor(Math.random() * files.length)];
+const percent = () => Math.floor(Math.random() * 100).toFixed(0);
+
 const token = "5703860431:AAGfBM38uS9xGIkLmZq3bcV1B-cm_6dDScQ";
 
 const bot = new TelegramApi(token, { polling: true });
@@ -22,18 +25,29 @@ const start = () => {
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
+    const leha = msg.from.username.toLowerCase() === "leshapain";
+
+    const findSteel = chosenFile().includes("jeleznij");
 
     if (text === START) {
       return await bot.sendMessage(chatId, "АУФ, ЧЕРТИ");
     }
 
-    if (text === CHECK) {
-      const chosenFile = files[Math.floor(Math.random() * files.length)];
-      const percent = Math.floor(Math.random() * 100).toFixed(0);
-
-      await bot.sendMessage(chatId, `Ты железный на ${percent}%`);
-      return await bot.sendPhoto(chatId, `public/images/${chosenFile}`);
+    if (text === CHECK && !leha && !findSteel) {
+      console.log(chosenFile());
+      console.log(findSteel);
+      await bot.sendMessage(chatId, `Ты железный на ${percent()}%`);
+      return await bot.sendPhoto(chatId, `public/images/${chosenFile()}`);
     }
+
+    if (leha || findSteel) {
+      console.log(chosenFile());
+      console.log(findSteel);
+
+      await bot.sendMessage(chatId, `Ты железный на 100%`);
+      return await bot.sendPhoto(chatId, `public/images/${chosenFile()}`);
+    }
+
     return await bot.sendMessage(chatId, "ты че волк???");
   });
 };
